1974602eb872c1240aef0016bbb9c050379218a7
José Luis Corcuera Bárcena <jlcorcuera@gmail.com>
2026-04-08 14:32:41 -0500

    Fibonacci's DP version

---
+++ cap-02-dynamic-programming/fibonacci/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+long long fibonacci(long long N) {
+    if (N == 0) {
+        return 0;
+    }
+    if (N == 1) return 1;
+    return fibonacci(N - 1) + fibonacci(N - 2);
+}
+
+int main() {
+    long long N = 40;
+    long long dp[N + 1];
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= N ; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    cout << "Fibonacci N DP: "<< dp[N] << endl;
+    cout << "Fibonacci N RECURSIVE: "<< fibonacci(N) << endl;
+    return 0;
+}
